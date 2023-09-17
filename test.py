39f4dd518f480ba0be0228d8bd6c2463379ede5b
caj <caj@163.com>
2023-09-17 16:19:01 +0800

    main test

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+
+def get_data(file_path):
+    """
+    :param file_path: 文件路径
+    :return: 返回一个dataframe
+    """
+    df = pd.read_csv(file_path, encoding='utf-8')
+    return df
+    
