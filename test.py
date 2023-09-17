e00c12703afb1bc02d9355262a2cfda028504ce5
caj <caj@163.com>
2023-09-17 16:20:27 +0800

    branch test

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 import pandas as pd
 import numpy as np
+
+
+def data(file_name):
+    """
+    Reads the data from the file and returns a dataframe
+    :param file_name: name of the file
+    :return: dataframe
+    """
+    df = pd.read_csv(file_name)
+    return df
